74b4d06faf7633fc31a42f82adf81d6d184ad8f8
NicolasRivasR <nicodesar@gmail.com>
2024-11-13 18:19:10 +0100

    La notificación sale debajo del formulario y no lo desplaza

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="styles.css">
     <title>Formulario de contacto</title>
@@ -39,11 +39,10 @@
             <input type="checkbox" id="aceptar_terminos" >
             <label for="aceptar_terminos">He leído y acepto la <span>politíca de privacidad</span></label>
         </div>
-        <div>    <input type="submit" id="Enviar" class="button">  </div>
+        <div>    <input type="submit" id="Enviar" class="button"> <p class="rep" id="respuesta"></p>  </div>
 
     </form>
     
-    <div class="rep" id="respuesta"> </div>
 </body>
 
 <script>
