bef3b114370293d0531970d86b6dddf23ad9d13a
NicolasRivasR <nicodesar@gmail.com>
2024-11-13 14:51:36 +0100

    Conexión a la base de datos con php

--- recogidaDeDatos.php
+++ recogidaDeDatos.php
@@ -1,17 +1,31 @@
 <?php 
 
-$nombre = $_GET["nombre"];
-$email = $_GET["email"];
-$telefono = $_GET["telefono"];
-$asunto = $_GET["asunto"];
-$comentarios = $_GET["comentarios"];
+include("conexionDB.php");
 
-echo "He recibido los siguietes datos: \n";
-echo $nombre . "\n";
-echo $email . "\n";
-echo $telefono. "\n";
-echo $asunto. "\n";
-echo $comentarios. "\n";
+if($conexion){
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $telefono = $_POST["telefono"];
+    $asunto = $_POST["asunto"];
+    $comentarios = $_POST["comentarios"];
+    
+    echo "He recibido los siguietes datos: \n";
+    echo $nombre . "\n";
+    echo $email . "\n";
+    echo $telefono. "\n";
+    echo $asunto. "\n";
+    echo $comentarios. "\n";
+    
+}
+else{
+
+    echo "No ha sido posible conectarse a la base de datos";
+
+}
+
+
+
+
 ?>
 
 
